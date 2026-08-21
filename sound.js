62ddfa1a4d9c63c9a8617e5db719e4df4cd5cd90
qrian-dot-cloud <qqqrian@gmail.com>
2026-08-21 16:15:31 +0100

    Update sound.js

--- sound.js
+++ sound.js
@@ -92,7 +92,7 @@ function playMenuBlip() {
 
   const masterFilter = audioCtx.createBiquadFilter();
   masterFilter.type = 'lowpass';
-  masterFilter.frequency.setValueAtTime(1000, now);
+  masterFilter.frequency.setValueAtTime(1400, now);
   masterFilter.connect(audioCtx.destination);
   masterFilter.connect(delay);
 
@@ -100,7 +100,7 @@ function playMenuBlip() {
   const bodyGain = audioCtx.createGain();
   body.type = 'sine';
 
-  const freq = 220 + (Math.random() - 0.5) * 30; // 175-205Hz random
+  const freq = 220 + (Math.random() - 0.5) * 110; //
   body.frequency.setValueAtTime(freq, now + 0.02);
 
   bodyGain.gain.setValueAtTime(0.0001, now + 0.02);
